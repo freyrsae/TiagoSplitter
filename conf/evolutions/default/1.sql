1f4f6e4cf1fe59457892515599def731fb6d265f
freyr <freyrsae@gmail.com>
2014-01-22 12:31:19 -0800

    prófa mig áfram með Heroku

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,7 +3,7 @@
 
 # --- !Ups
 
-create table `contacts` (`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,`kennitala` VARCHAR(254) NOT NULL,`name` VARCHAR(254) NOT NULL,`userEmail` VARCHAR(254) NOT NULL,`drasl` VARCHAR(254) NOT NULL);
+create table `contacts` (`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,`kennitala` VARCHAR(254) NOT NULL,`name` VARCHAR(254) NOT NULL,`userEmail` VARCHAR(254) NOT NULL);
 create table `demands` (`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,`user_email` VARCHAR(254) NOT NULL,`amount` INTEGER NOT NULL,`perall` VARCHAR(254) NOT NULL,`description` VARCHAR(254) NOT NULL,`recipients` VARCHAR(254) NOT NULL,`status` VARCHAR(254) NOT NULL);
 create table `users` (`email` VARCHAR(254) NOT NULL PRIMARY KEY,`name` VARCHAR(254) NOT NULL,`pass` VARCHAR(254) NOT NULL,`role` VARCHAR(254));
 alter table `contacts` add constraint `user_contact_fk` foreign key(`userEmail`) references `users`(`email`) on update NO ACTION on delete NO ACTION;
